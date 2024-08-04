ae06dd0d152d8ef6a52bab8a7bd86e06f944bb9b
KindaSloth <guilhermereis28122002@gmail.com>
2024-08-04 18:04:43 -0300

    feat: transaction repo

--- repository/transaction.go
+++ repository/transaction.go
@@ -1,9 +1,96 @@
 package repository
 
 import (
+	"welloff-bank/model"
+
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
+	"github.com/shopspring/decimal"
 )
 
 type TransactionRepository struct {
 	Pg *sqlx.DB
+}
+
+func (tr *TransactionRepository) GetTransaction(transaction_id string) (*model.Transaction, error) {
+	transaction := new(model.Transaction)
+	err := tr.Pg.Get(
+		transaction,
+		`SELECT tx.id, tx.kind, tx.from_account_id, tx.to_account_id, tx.amount, tx.date_issued, tx.related_transaction_id, tx.created_at, tx.updated_at
+		FROM "transaction" tx WHERE tx.id = $1`,
+		transaction_id,
+	)
+
+	return transaction, err
+}
+
+func (tr *TransactionRepository) GetTransactionsByAccount(account_id string, limit int, offset int) (*[]model.Transaction, error) {
+	transactions := new([]model.Transaction)
+	err := tr.Pg.Select(
+		transactions,
+		`
+		SELECT 
+			tx.id, tx.kind, tx.from_account_id, tx.to_account_id, tx.amount, tx.date_issued, tx.related_transaction_id, tx.created_at, tx.updated_at
+		FROM 
+			"transaction" tx 
+		WHERE 
+			tx.from_account_id = $1
+		ORDER BY
+			tx.date_issued DESC
+		LIMIT 
+			$2
+		OFFSET 
+			$3
+		`,
+		account_id,
+		limit,
+		offset,
+	)
+
+	return transactions, err
+}
+
+func (tr *TransactionRepository) GetTransactionsByDate(account_id string, date_from string, date_to string, limit int, offset int) (*[]model.Transaction, error) {
+	transactions := new([]model.Transaction)
+	err := tr.Pg.Select(
+		transactions,
+		`
+		SELECT 
+			tx.id, tx.kind, tx.from_account_id, tx.to_account_id, tx.amount, tx.date_issued, tx.related_transaction_id, tx.created_at, tx.updated_at
+		FROM 
+			"transaction" tx 
+		WHERE 
+			tx.from_account_id = $1
+		AND 
+			tx.date_issued BETWEEN $2 AND $3
+		ORDER BY
+			tx.date_issued DESC
+		LIMIT 
+			$4
+		OFFSET 
+			$5
+		`,
+		account_id,
+		date_from,
+		date_to,
+		limit,
+		offset,
+	)
+
+	return transactions, err
+}
+
+func (tr *TransactionRepository) CreateTransaction(transaction_id uuid.UUID, kind string, from_account_id *string, to_account_id *string, amount decimal.Decimal, related_transaction_id *string) error {
+	_, err := tr.Pg.Exec(
+		`INSERT INTO "transaction" (id, kind, from_account_id, to_account_id, amount, related_transaction_id)
+		VALUES ($1, $2, $3, $4, $5, $6)`,
+		transaction_id,
+		kind,
+		from_account_id,
+		to_account_id,
+		amount,
+		related_transaction_id,
+	)
+
+	return err
 }
