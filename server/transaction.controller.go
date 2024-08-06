2d34bf36bf80fbca33f37629d02c1df354c0b27b
KindaSloth <guilhermereis28122002@gmail.com>
2024-08-05 21:28:14 -0300

    feat: refund endpoint impl

--- server/transaction.controller.go
+++ server/transaction.controller.go
@@ -228,7 +228,79 @@ func (s *Server) RefundTransaction() gin.HandlerFunc {
 			return
 		}
 
-		// TODO
-		ctx.Status(400)
+		transaction, err := s.Repositories.TransactionRepository.GetTransaction(transaction_id)
+		if err != nil {
+			log.Println("[ERROR] [RefundTransaction] failed to get transaction: ", err)
+			ctx.JSON(404, gin.H{"error": "Transaction not found"})
+			return
+		}
+
+		if transaction.Kind != "transfer" {
+			ctx.JSON(400, gin.H{"error": "Transaction cannot be refunded"})
+			return
+		}
+
+		user, err := utils.GetUser(ctx)
+		if err != nil {
+			log.Println("[ERROR] [RefundTransaction] failed to get user from context: ", err)
+			ctx.Status(401)
+			return
+		}
+
+		from_account, err := s.Repositories.AccountRepository.GetAccount(transaction.FromAccountId.String())
+		if err != nil {
+			log.Println("[ERROR] [RefundTransaction] failed to get account: ", err)
+			ctx.JSON(404, gin.H{"error": "Account not found"})
+			return
+		}
+
+		to_account, err := s.Repositories.AccountRepository.GetAccount(transaction.ToAccountId.String())
+		if err != nil {
+			log.Println("[ERROR] [RefundTransaction] failed to get account: ", err)
+			ctx.JSON(404, gin.H{"error": "Account not found"})
+			return
+		}
+
+		if (from_account.UserId.String() != user.Id.String()) && (to_account.UserId.String() != user.Id.String()) {
+			ctx.JSON(401, gin.H{"error": "User is not the owner of any of the accounts"})
+			return
+		}
+
+		to_account_balance, err := utils.GetAccountBalance(ctx, to_account.Id, s.Repositories)
+		if err != nil {
+			log.Println("[ERROR] [RefundTransaction] failed to get account balance: ", err)
+			ctx.JSON(500, gin.H{"error": "Failed to complete refund transaction"})
+			return
+		}
+
+		if to_account_balance.Balance.LessThan(transaction.Amount) {
+			ctx.JSON(400, gin.H{"error": "Insufficient balance"})
+			return
+		}
+
+		refund_transaction_id, err := uuid.NewV7()
+		if err != nil {
+			log.Println("[ERROR] [TransferTransaction] failed to create transaction id: ", err)
+			ctx.JSON(500, gin.H{"error": "Failed to complete transfer transaction"})
+			return
+		}
+
+		tx, err := s.Repositories.TransactionRepository.GetTransaction(refund_transaction_id.String())
+		if tx != nil && err == nil {
+			ctx.JSON(500, gin.H{"error": "Duplicated transaction"})
+			return
+		}
+
+		from_account_id := transaction.FromAccountId.String()
+		to_account_id := transaction.ToAccountId.String()
+
+		err = s.Repositories.TransactionRepository.CreateTransaction(refund_transaction_id, "refund", &from_account_id, &to_account_id, transaction.Amount, nil)
+		if err != nil {
+			log.Println("[ERROR] [RefundTransaction] failed to create transaction: ", err)
+			ctx.JSON(500, gin.H{"error": "Failed to complete refund transaction"})
+			return
+		}
+
+		ctx.Status(200)
 	}
 }
